28712df7dfe537b4860f7dc2bedd938a7788ee43
As71er <216588669+As71er@users.noreply.github.com>
2026-04-29 17:12:58 -0500

    [ADD] prod frontend CORS

--- app/main.py
+++ app/main.py
@@ -55,7 +55,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        'https://prod-frontendusb.vercel.app/',
+        'https://frontend-usblens.vercel.app',
     ],
     allow_credentials=True,
     allow_methods=["*"],
