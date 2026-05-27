0a34c72df33d4a09ae47ec9dd89c309db3d8b7eb
As71er <216588669+As71er@users.noreply.github.com>
2026-05-26 21:17:04 -0500

    [ADD] vercel front CORS

--- app/main.py
+++ app/main.py
@@ -53,9 +53,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        'https://frontend-usblens.vercel.app',
+        'https://usblens.vercel.app',
     ],
     allow_credentials=True,
     allow_methods=["*"],
