0119ac56c13c7a5d810b8f27d42ff87f9298778a
As71er <216588669+As71er@users.noreply.github.com>
2026-06-02 23:59:13 -0500

    [ADD] allow frontend

--- app/main.py
+++ app/main.py
@@ -53,8 +53,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
+        "usblens.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
